7e12ed0dbdd0cc491f85877c25560cd629b82d20
sonndl <sonndl@vng.com.vn>
2026-01-16 11:50:24 +0700

    test deploy dev has source 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+  plugins: [react()],
+
   server: {
     host: true,
-    allowedHosts: "all",
-  },
-});
+    port: 5173,
+    allowedHosts: [
+      'prosimex.pitundev.io.vn',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
+})
