df0e55edf4aea1000e17b2f9a2856e4eb16f8cc8
sonndl <sonndl@vng.com.vn>
2026-01-16 15:54:24 +0700

    update follow deploy final 7

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log("Backend running on port 3000");
+  console.log("Backend running on port 3000 - SOnNDL");
 });
