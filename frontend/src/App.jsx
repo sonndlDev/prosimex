c66f27465f228e8b5dac8bd3d64a6e34d3753c9f
sonndl <sonndl@vng.com.vn>
2026-01-16 15:29:19 +0700

    update follow deploy dev fe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Bùi Thị Huyền Thật</h1>
+      <h1>test cicd FE</h1>
     </>
   )
 }
