2c3f04991c21800a24bd9e675927368abb4957c7
sonndl <sonndl@vng.com.vn>
2026-01-16 11:37:55 +0700

    test deploy dev has source 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
-        server: {
-          host: true,
-          allowedHosts: "all",
-        },
       },
     }),
   ],
+  server: {
+    host: true,
+    allowedHosts: "all",
+  },
 });
