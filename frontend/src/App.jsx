3a04dd6b79b149972ce200983ced5e967c83786f
sonndl <sonndl@vng.com.vn>
2026-01-16 14:51:06 +0700

    test commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Dev Prosimex Env</h1>
+      <h1>Bùi Thị Huyền Thật</h1>
     </>
   )
 }
