fd6c4134dbd9eb54a3d16ce2fd3450dc01e8eef8
sonndl <sonndl@vng.com.vn>
2026-01-16 15:43:11 +0700

    update follow deploy final 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Production test</h1>
+      <h1>Production tests</h1>
     </>
   )
 }
