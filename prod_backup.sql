054958d9ec26fa4570fd73f7fb9bf6f3b5e928f1
SonNDL <deploy@srv1272652.hstgr.cloud>
2026-05-30 06:28:28 +0000

    update prod coòonfig db

---
+++ prod_backup.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 1ZPimLILHlg84aJmM8GeMs1DJRxNWZMuS8MueEa0O7Jng6IoGxdViEQZNvkihp2
+
+-- Dumped from database version 15.15 (Debian 15.15-1.pgdg13+1)
+-- Dumped by pg_dump version 15.15 (Debian 15.15-1.pgdg13+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 1ZPimLILHlg84aJmM8GeMs1DJRxNWZMuS8MueEa0O7Jng6IoGxdViEQZNvkihp2
+
